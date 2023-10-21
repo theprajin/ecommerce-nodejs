afd154c2e5b82fb61330fc85856cf3be3aa0e5c0
theprajin <prazenthekiller@gmail.com>
2023-10-21 12:46:20 +0545

    signed access token added

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
 
+// creates access token
 const createJWT = (payload) => {
   const token = jwt.sign(payload, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_LIFETIME,
@@ -7,17 +8,20 @@ const createJWT = (payload) => {
   return token;
 };
 
+// checks if the token for authentication is valid
 const isTokenMatch = (token) => {
   const isMatch = jwt.verify(token, process.env.JWT_SECRET);
   return isMatch;
 };
 
+// attaches the token as http-only cookies
 const attachCookiesToResponse = ({ res, tokenUser }) => {
   const token = createJWT(tokenUser);
   res.cookie("token", token, {
     httpOnly: true,
     expires: new Date(Date.now() + 1000 * 60 * 60 * 24),
     secure: process.env.NODE_ENV === "production",
+    signed: true,
   });
 };
 
