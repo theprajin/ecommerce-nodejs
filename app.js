2a1f3a298f0ba988b9df2ae6e79b4b08cb035d1d
theprajin <prazenthekiller@gmail.com>
2023-11-02 15:10:35 +0545

    swagger doc bug fixed

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(cookieParser(process.env.JWT_SECRET));
 app.use(express.static("./public"));
 app.use(fileUpload());
 
-app.use("/api/docs", swaggerUI.serve, swaggerUI.setup(swaggerDoc));
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 
 app.get("/api/v1", (req, res) => {
   console.log(req.cookies);
