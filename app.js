34a7a83bb9d08a411bcdcd12786bbef3f830159e
theprajin <prazenthekiller@gmail.com>
2023-11-02 12:43:43 +0545

    review api route added

--- app.js
+++ app.js
@@ -10,13 +10,14 @@ const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const fileUpload = require("express-fileupload");
 
+//database
+const connectDB = require("./db/connect");
+
 //routers
 const authRouter = require("./routes/authRoutes");
 const userRouter = require("./routes/userRoutes");
 const productRouter = require("./routes/productRoutes");
-
-//database
-const connectDB = require("./db/connect");
+const reviewRouter = require("./routes/reviewRoutes");
 
 //middleware
 const notFoundMiddleware = require("./middleware/not-found");
@@ -37,6 +38,7 @@ app.get("/api/v1", (req, res) => {
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/products", productRouter);
+app.use("/api/v1/reviews", reviewRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
