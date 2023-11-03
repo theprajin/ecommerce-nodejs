252d57fa55c9566fc499f846a59d501bae3ff583
theprajin <prazenthekiller@gmail.com>
2023-11-03 12:52:37 +0545

    order api and docgen added

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ const authRouter = require("./routes/authRoutes");
 const userRouter = require("./routes/userRoutes");
 const productRouter = require("./routes/productRoutes");
 const reviewRouter = require("./routes/reviewRoutes");
+const orderRouter = require("./routes/orderRoutes");
 
 //middleware
 const notFoundMiddleware = require("./middleware/not-found");
@@ -44,7 +45,7 @@ app.use(
 app.use(helmet());
 app.use(cors());
 app.use(xss());
-app.use(mongoSanitize());
+// app.use(mongoSanitize());
 
 app.use(morgan("tiny"));
 app.use(express.json());
@@ -65,15 +66,20 @@ app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/products", productRouter);
 app.use("/api/v1/reviews", reviewRouter);
+app.use("/api/v1/orders", orderRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 const start = async () => {
-  await connectDB(process.env.MONGO_URL);
-  console.log("Successfully conneted to the database");
-  app.listen(port, console.log(`Server is listening on port: ${port}`));
+  try {
+    await connectDB(process.env.MONGO_URL);
+    console.log("Successfully conneted to the database");
+    app.listen(port, console.log(`Server is listening on port: ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 start();
